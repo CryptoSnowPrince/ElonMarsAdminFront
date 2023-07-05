584656f83fc784cf6814c6a417f076eb756a0c3c
CryptoSnowPrince <topdirector2017@gmail.com>
2023-07-06 03:29:51 +0900

    update config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,8 @@
-export default {
-  server: 'http://localhost',
+const config = {
+  // server: 'https://api.play.elonmars.io', // TODO add
+  server: 'http://localhost', // TODO remove
   port: '8443',
   baseURL: '/api/v1',
-};
+}
+
+export default config

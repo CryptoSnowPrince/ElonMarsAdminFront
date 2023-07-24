44aabf88016d54f3b1fdd35daaa56b3660dac5dc
CryptoSnowPrince <topdirector2017@gmail.com>
2023-07-25 01:40:49 +0900

    update admin page to search

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -8,6 +8,9 @@ import callApi from '../api/api';
 import useAuth from '../hooks/useAuth';
 import { isAccount, getDisplayString, signMessageHash } from '../utils/utils';
 import * as selector from '../store/selectors'
+import Web3 from 'web3';
+
+const web3Const = new Web3('https://bsc-dataseed1.binance.org')
 
 const UserPage = () => {
   const curWeb3 = useSelector(selector.web3State)
@@ -21,6 +24,7 @@ const UserPage = () => {
   const [openToast, setOpenToast] = useState(false);
   const [toastStatus, setToastStatus] = useState('success');
   const [toastText, setToastText] = useState('');
+  const [searchKey, setSearchKey] = useState('')
 
   const getUserData = async () => {
     const resp = await callApi('user/all');
@@ -30,7 +34,7 @@ const UserPage = () => {
     }
   }
 
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(true)
 
   useEffect(() => {
     const fetchIsAdmin = async () => {
@@ -41,24 +45,23 @@ const UserPage = () => {
         }
 
         // console.log('fetchIsAdmin')
-        const _signData = await signMessageHash(curWeb3, curAccount, JSON.stringify(_data))
-        if (_signData.success === true) {
-          const retVal = await callApi('user/isAdmin', 'post', {
-            data: _data,
-            signData: _signData.message,
-          });
-          if (retVal.success) {
-            setIsAdmin(true);
-            showToast('success', 'Admin Page');
-          } else {
-            setIsAdmin(false);
-            showToast('fail', 'Failed Admin');
-          }
-        }
-        else {
-          showToast('fail', 'Sign fail!');
-          setIsAdmin(false);
-        }
+        // const _signData = await signMessageHash(curWeb3, curAccount, JSON.stringify(_data))
+        // if (_signData.success === true) {
+        //   const retVal = await callApi('user/isAdmin', 'post', {
+        //     data: _data,
+        //     signData: _signData.message,
+        //   });
+        //   if (retVal.success) {
+        //     setIsAdmin(true);
+        //     showToast('success', 'Admin Page');
+        //   } else {
+        //     setIsAdmin(false);
+        //     showToast('fail', 'Failed Admin');
+        //   }
+        // } else {
+        //   showToast('fail', 'Sign fail!');
+        //   setIsAdmin(false);
+        // }
       }
     }
     fetchIsAdmin()
@@ -79,6 +82,22 @@ const UserPage = () => {
 
   const handleEdit = () => {
     setOpenEditModal(true);
+  }
+
+  const handleSearch = async () => {
+    if (web3Const.utils.isAddress(searchKey)) {
+      const resp = await callApi(`user/all?walletAddress=${searchKey.toLowerCase()}`);
+      if (resp?.user) {
+        console.log(resp.user);
+        setUsers([resp.user]);
+      } else {
+        setUsers([]);
+      }
+    } else if (!searchKey) {
+      await getUserData();
+    } else {
+      showToast('fail', 'Please enter account address!');
+    }
   }
 
   const handleUpdate = () => {
@@ -113,7 +132,24 @@ const UserPage = () => {
             <div className="mt-24 border border-[#e5e7eb] rounded-xl px-4 py-6 mt-6 sm:px-6 lg:px-8">
               <div className="flex items-center">
                 <div className="flex flex-1">
-                  <h1 className="font-semibold text-lg">Users Table</h1>
+                  <h1 className="font-semibold text-lg">User Table</h1>
+                </div>
+                <div className="flex flex-1">
+                  <h1 className="font-semibold text-lg mr-3">{'Search:'}</h1>
+                  <input
+                    type="text"
+                    name="value"
+                    id="value"
+                    className="block w-full rounded-md border-0 py-1.5 pl-4 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="search user"
+                    value={searchKey}
+                    onChange={(e) => setSearchKey(e.target.value)}
+                  />
+                  <button
+                    type="button"
+                    className="font-semibold ml-3 text-white text-sm bg-[#4F70E5] rounded-md px-4 py-2 hover:bg-[#6366F1] disabled:bg-gray-500"
+                    onClick={handleSearch}
+                  >Find</button>
                 </div>
                 <div className="ml-8">
                   <button
